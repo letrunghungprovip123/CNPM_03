142a6d0e04e12996264a416d467cf2a8c38793e4
letrunghungprovip123 <hle01678z@gmail.com>
2025-03-24 11:02:06 +0700

    update

--- Test.java
+++ Test.java
@@ -2,7 +2,9 @@ public class Test {
     public static void main(String[] args) {
         People p1 = new People("John", 20, 180, 70);
         Student st = new Student("Jane", 21, 170, 60, "Computer Science", 20210001);
+        Employee e = new Employee("Tom", 30, 175, 75, "IT", 20210002);
         System.out.println(p1.toString());
         System.out.println(st.toString());
+        System.out.println(e.toString());
     }
 }
